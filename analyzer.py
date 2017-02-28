415cc7f9bdbae74a5d38bd8c56fb196cf59cfafa
praveen <praveen049@yaoo.com>
2017-02-28 18:24:21 +0800

    Compiled

--- analyzer.py
+++ analyzer.py
@@ -1,19 +1,25 @@
-import tensorflow as flow
+import tensorflow as tf
 import sys
+import argparse
 from tensorflow.python.platform import gfile
 
-
 def create_images(video_file, image_dir, image_count):
+    print ("inside create images")
 
-def main(_)
+def label_images(image_list):
+    print ("inside lavel images")
+    
+def main(_):
   if not tf.gfile.Exists(FLAGS.video_file):
      print ("Video file '" + FLAGS.video_file + "'does not exist")
      return None
   if not tf.gfile.Exists(FLAGS.image_dir):
      print ("Image directory '" + FLAGS.image_dir + "' does not exist")
      return None
-  image_lists = create_images(FLAGS.video_file, FLAGS.image_dir,
+  image_list = create_images(FLAGS.video_file, FLAGS.image_dir,
                               FLAGS.images)
+  label_list = label_images(image_list)
+ 
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
@@ -23,7 +29,7 @@ if __name__ == '__main__':
       default='',
       help='The path to the output image files.'
   )
-  parser.add_image(
+  parser.add_argument(
       '--video_file',
       type=str,
       default='',
